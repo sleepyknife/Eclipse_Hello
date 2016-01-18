d4ce277f45a43e335e343aad5de0de219c70bfd5
21524677 <sleepycat7912@gmail.com>
2016-01-18 17:18:30 +0800

    second one text

--- Eclipse_Hello/src/com/example/eclipse_hello/MainActivity.java
+++ Eclipse_Hello/src/com/example/eclipse_hello/MainActivity.java
@@ -13,6 +13,6 @@ public class MainActivity extends AppCompatActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		TextView mytext = (TextView) findViewById(R.id.mytextview);
-		mytext.setText("jadklsjdlsd");
+		mytext.setText("second one");
 	}
 }
