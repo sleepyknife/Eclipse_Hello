99148ada4ea3bb89e73bc80b194e3fb0637e6e43
21524677 <sleepycat7912@gmail.com>
2016-01-19 11:58:26 +0800

    version four

--- Eclipse_Hello/src/com/example/eclipse_hello/MainActivity.java
+++ Eclipse_Hello/src/com/example/eclipse_hello/MainActivity.java
@@ -13,6 +13,6 @@ public class MainActivity extends AppCompatActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		TextView mytext = (TextView) findViewById(R.id.mytextview);
-		mytext.setText("third one");
+		mytext.setText("version four");
 	}
 }
